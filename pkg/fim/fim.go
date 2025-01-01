8f4e87f9b5f99c9f9469e0328401dd97d8b24200
Faheem <faheem5948@gmail.com>
2025-01-01 14:12:57 -0500

    recursively generate hashes for all files in dir
    Signed-off-by: Faheem <faheem5948@gmail.com>

--- pkg/fim/fim.go
+++ pkg/fim/fim.go
@@ -1,7 +1,93 @@
 package fim
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+)
+
+var symlinkTargets = make(map[string]string)
+var mu sync.Mutex
 
 func BuildHashDB(rootDir string) {
 	fmt.Printf("Building Hash DB for %s\n", rootDir)
+
+	err := filepath.WalkDir(rootDir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Generate hash for files
+		if !d.IsDir() && d.Type() != fs.ModeSymlink {
+			hash, err := generateSHA256Hash(path)
+			if err != nil {
+				log.Printf("Error generating hash for %s: %v\n", path, err)
+				return nil
+			}
+			// fmt.Printf("%s:\t%x\n", path, hash)
+		}
+
+		// Iterate over Symlink directories
+		if d.Type() == fs.ModeSymlink {
+			fmt.Printf("%s is a symlink.\n", path)
+			// Resolve the symbolic link to get the actual file path
+			targetPath, err := os.Readlink(path)
+			if err != nil {
+				log.Printf("Error reading symbolic link %s: %v\n", path, err)
+				return nil
+			}
+
+			// Handle cases where targetPath does not start with "/"
+			if !strings.HasPrefix(targetPath, "/") {
+				targetPath = filepath.Join(filepath.Dir(path), targetPath)
+			}
+
+			// Check if the targetPath has already been processed
+			mu.Lock()
+			_, exists := symlinkTargets[targetPath]
+			mu.Unlock()
+
+			if !exists {
+				mu.Lock()
+				symlinkTargets[targetPath] = targetPath
+				mu.Unlock()
+
+				BuildHashDB(targetPath) // recursive call
+
+			} else {
+				log.Printf("Skipping duplicate symlink target: %s\n", targetPath)
+			}
+
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("Error building hash db for %s: %v\n", rootDir, err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Finished Generating Hash for %s.\n", rootDir)
+}
+
+func generateSHA256Hash(filePath string) ([]byte, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	hash := sha256.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return nil, err
+	}
+
+	return hash.Sum(nil), nil
 }
